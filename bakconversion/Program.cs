eae5eb5c70bc6a87a9b02a9dd13e05636448c07a
Samuel Neff <neff.samuelg@gmail.com>
2019-06-06 16:05:24 -0500

    Add bak conversion files

---
+++ bakconversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static Restorinator;
+
+namespace bakconversion {
+    class Program {
+        static void Main(string[] args) {
+        string filePath = @"C:\Fifth_Partners_backup.bak";
+        string databaseName = "Fifth_Partners";
+        string serverName = "DESKTOP-IP5TIF2\\SQLEXPRESS";
+        string dataFilePath = @"C:\";
+        string logFilePath = @"C:\Users\neffs\OneDrive\Documents\sqlserverwork\";
+
+        Console.Out.WriteLine("is this working?");
+            Restorinator restoreEngine = new Restorinator();
+            restoreEngine.RestoreDatabase(databaseName, filePath, serverName,
+        " ", " ", dataFilePath, logFilePath);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ bakconversion/Restorinator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+//using Microsoft.SqlServer.Management.Sdk.Sfc;
+
+public class Restorinator
+{
+
+    public void RestoreDatabase(String databaseName, String filePath, String serverName,
+                String userName, String password,
+                String dataFilePath, String logFilePath)
+    {
+        Restore sqlRestore = new Restore();
+
+        BackupDeviceItem deviceItem = new BackupDeviceItem(filePath, DeviceType.File);
+        sqlRestore.Devices.Add(deviceItem);
+        sqlRestore.Database = databaseName;
+
+        ServerConnection connection = new ServerConnection(serverName);
+        //sqlServer.KillAllProcesses(databaseName);
+        Server sqlServer = new Server(connection);
+
+        Database db = sqlServer.Databases[databaseName];
+        sqlRestore.Action = RestoreActionType.Database;
+        String dataFileLocation = dataFilePath + databaseName + ".mdf";
+        String logFileLocation = logFilePath + databaseName + "_Log.ldf";
+
+        db = sqlServer.Databases[databaseName];
+
+
+        sqlRestore.RelocateFiles.Add(new RelocateFile(databaseName, dataFileLocation));
+        sqlRestore.RelocateFiles.Add(new RelocateFile(databaseName + "_log", logFileLocation));
+        sqlRestore.ReplaceDatabase = true;
+        sqlRestore.Complete += new ServerMessageEventHandler((sender, e) => {
+            Console.WriteLine("\nThe dishes are done");
+        });
+        sqlRestore.PercentCompleteNotification = 10;
+        sqlRestore.PercentComplete +=
+           new PercentCompleteEventHandler((sender1, e1) => {
+               Console.Write(".");
+           });
+
+        sqlRestore.SqlRestore(sqlServer);
+        db = sqlServer.Databases[databaseName];
+        db.SetOnline();
+        sqlServer.Refresh();
+    }
+}
